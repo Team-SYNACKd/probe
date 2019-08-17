4cfa5ce62c0e966afc3c9a60f90f14cffda681e2
Aniketh01 <anikethgireesh@gmail.com>
2019-08-17 11:56:00 +0530

    Initial commit illustrating the layout

---
+++ probe.go
@@ -0,0 +1,8 @@
+package probe
+
+import "fmt"
+
+
+func main() {
+	fmt.Print("This should be the entry point for the symbolic execution. This creates the binary which lets any application use probe for TCP/IP purposes")
+}
